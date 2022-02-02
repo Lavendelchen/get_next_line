f1375cf0330418ae90e56761f5ee72e700f79132
Lavendelchen <lavendel02sh@gmail.com>
2022-02-02 03:31:19 +0100

    its messy lol

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,23 @@
 /*   By: shaas <shaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 16:21:14 by shaas             #+#    #+#             */
-/*   Updated: 2021/09/21 16:22:25 by shaas            ###   ########.fr       */
+/*   Updated: 2022/01/30 19:31:28 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+char	*ft_free_helper(char **str1, char **str2, char **str3);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_linejoin(char *old_line, char *add);
+int		ft_find_newline(char *buffer);
 char	*get_next_line(int fd);
 
-# endif
+
+#endif
